75a152c717cafb3319bf54cb6d787f1d8ff67676
Payut Pantawongdecha <payut.pan@gmail.com>
2013-04-22 14:36:23 -0400

    create rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Starfleet::Application.config.secret_token = '96ea7bd4ff4e5677c0e67bc6339d6bae1ebf0b9ebefba5efa94518b724d0074602a21b0ce193280a32fceaf99aa9d102bc80616d002f77b6cd34e88a647ffeae'
