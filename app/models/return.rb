f324232fcbe5bdba43892a89ee471066ff90e967
Sarah Edris <sedris@mit.edu>
2013-04-28 21:12:10 -0400

    moved reminders to returns

--- app/models/return.rb
+++ app/models/return.rb
@@ -5,12 +5,4 @@ class Return < ActiveRecord::Base
   has_one :reminder
   belongs_to :user 
 
-  after_create :create_default
-
-  # create blank reminder automatically
-  def create_default
-  	self.create_reminder
- 	self.reminder.sent = 0
- 	self.reminder.save
-  end
 end
