9cd44960e4ec56b07c7356895bbd5194e3b69b55
Payut Pantawongdecha <payut.pan@gmail.com>
2013-04-29 15:23:43 -0400

    add auto complete

--- app/views/returns/_form.js
+++ app/views/returns/_form.js
@@ -1,5 +1,10 @@
 <script type="text/javascript">
 $(function() {
-	$(".chzn-select").chosen();
+	//$(".chzn-select").attr("data-placeholder", "select email..");
+	//$(".chzn-select").chosen({ max_selected_options: 1 });
+	var tags = <%= @people.map{ |i| i.email}.to_json.html_safe %>;
+	$("#to_email").autocomplete({
+		source: tags
+	});
 });
 </script>
