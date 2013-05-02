58c6d9722818a260bd2363a61e04ab9b7633a00d
Sarah Edris <sedris@mit.edu>
2013-05-02 14:28:06 -0400

    can add item reports for open lends

---
+++ app/models/item_report.rb
@@ -0,0 +1,3 @@
+class ItemReport < ActiveRecord::Base
+  attr_accessible :description, :item_id, :user_id
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130501182330) do
+ActiveRecord::Schema.define(:version => 20130502171612) do
+
+  create_table "item_reports", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "item_id"
+    t.string   "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "items", :force => true do |t|
     t.string   "name"
