6f64463ff1a047cff6c652fd8db869d678fd1bca
Stephanie Gu <steph.gu@gmail.com>
2013-05-07 01:59:21 -0400

    changed the gemfile so uploading profile pics work

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130506185511) do
+ActiveRecord::Schema.define(:version => 20130507015548) do
 
   create_table "item_reports", :force => true do |t|
     t.integer  "user_id"
@@ -29,17 +29,17 @@ ActiveRecord::Schema.define(:version => 20130506185511) do
     t.datetime "updated_at", :null => false
   end
 
-  create_table "items_tags", :id => false, :force => true do |t|
-    t.integer "item_id"
-    t.integer "tag_id"
-  end
-
   create_table "lends", :force => true do |t|
     t.string   "status"
     t.integer  "to_id"
     t.integer  "user_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "lends_tags", :id => false, :force => true do |t|
+    t.integer "lend_id"
+    t.integer "tag_id"
   end
 
   create_table "reminders", :force => true do |t|
