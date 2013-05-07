d16b09fed2226325ca621ea1a2c40f34d47f1cfd
Payut Pantawongdecha <payut.pan@gmail.com>
2013-05-07 02:32:33 -0400

    add stuff

--- app/controllers/lends_controller.rb
+++ app/controllers/lends_controller.rb
@@ -62,7 +62,8 @@ class LendsController < ApplicationController
   # POST /lends
   # POST /lends.json
   def create
-    @lend = current_user.lends.new({:status => "open"})
+    @lend = current_user.lends.create(params[:lend])
+    @lend.update_attributes({:status => "open"})
     @lend.build_item({:name => params[:item_name], :lend_id => current_user.id})
     tags = params[:tags].split(",")
     tags.each do |tag|
