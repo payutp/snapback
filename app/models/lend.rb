a875dff9c747a7a7214f4afcaad7568ef5bb1a65
Payut Pantawongdecha <payut.pan@gmail.com>
2013-05-08 00:55:38 -0400

    x

--- app/models/lend.rb
+++ app/models/lend.rb
@@ -1,5 +1,5 @@
 class Lend < ActiveRecord::Base
-  attr_accessible :status, :to_id, :user_id, :photo
+  attr_accessible :status, :to_id, :user_id, :photo, :photo_file_name
   has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "30x30>" },
         :default_url => "/images/:style/missing2.png",
         :url  => "/assets/lends/:id/:style/:basename.:extension",
