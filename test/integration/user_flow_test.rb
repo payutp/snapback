959726ff632ad63859c997ee0131cdf825842e27
Stephanie Gu <steph.gu@gmail.com>
2013-05-13 00:15:42 -0400

    added integration tests

--- test/integration/user_flow_test.rb
+++ test/integration/user_flow_test.rb
@@ -12,14 +12,4 @@ class UserFlowTest < ActionDispatch::IntegrationTest
 		xhr :delete, "/sessions/:id" 
 		assert_response 302
 	end
-	setup do
-		@u1 = User.create(:email=>"a@mit.edu", :password=>"a")
-    @u2 = User.create(:email=>"b@mit.edu", :password=>"ta")
-	end
-
-  test "general flow" do
-  	post_via_redirect "/login"
-  	assert_equal '/', path
-  	assert_equal @u1.id, session[:user_id]
-  end
 end
