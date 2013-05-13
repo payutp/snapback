f0929d940d7ea01e4beeb3127c7b5f46e8f4d9b6
mjhu <mjhu91@gmail.com>
2013-05-12 23:46:24 -0400

    1 successful items test

--- test/functional/items_controller_test.rb
+++ test/functional/items_controller_test.rb
@@ -2,16 +2,32 @@ require 'test_helper'
 
 class ItemsControllerTest < ActionController::TestCase
   setup do
-    @item = Item.new
-    @lend = Lend.new
-    @return = Return.new
-    @item.lend = @lend
+    @user = User.create(:email => 'foo@mit.edu', :password => 'bar', :password_confirmation => 'bar')
+    session[:user_id] = @user.id
+    @item = Item.create(:name => "test", :lend_id => @user.id)
+    @lend = @user.lends.create(:status => "pending", :to_id => @user.id, :user_id => @user.id)
+    @lend.id = 1
+    @return = Return.create(:status => "pending", :to_id => @user.id, :user_id => @user.id)
+    @lend.item = @item
     @item.return = @return
+
+    @item.save
+  end
+
+  teardown do
+    @user.delete
+    @item.delete
+    @lend.delete
+    @return.delete
   end
 
   test "should return item" do
     post :return, {:id => @item.id}
-
     assert_redirected_to activity_path
   end
+
+  test "should confirm item" do
+    post :return, {:id => @item.id}
+    assert_redirected_to activity_path
+  end  
 end
