12b79eee6667c342ff51a89c7668b92db3b25f0c
Stephanie Gu <steph.gu@gmail.com>
2013-05-13 00:06:36 -0400

    add integration test

--- test/integration/user_flow_test.rb
+++ test/integration/user_flow_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class UserFlowTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = User.create(:email => 'steph', :password => 'a')
+  end
+
+	test "login and then logout" do
+		xhr :post, "/sessions", { :email => 'steph', :password => 'a' }
+		assert_response 200
+
+		xhr :delete, "/sessions/:id" 
+		assert_response 302
+	end
 end
