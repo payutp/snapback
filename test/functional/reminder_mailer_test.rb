2aadb6fb6c707b39828aa820b280bf4baf8ee2c3
Sarah Edris <sredris@gmail.com>
2013-05-12 23:16:02 -0400

    test reminder

--- test/functional/reminder_mailer_test.rb
+++ test/functional/reminder_mailer_test.rb
@@ -4,5 +4,23 @@ class ReminderMailerTest < ActionMailer::TestCase
   # test "the truth" do
   #   assert true
   # end
-  
+  setup do
+    @user_to = User.create(:email=>"a@mit.edu", :password=>:"test")
+    @user_from = User.create(:email=>"b@mit.edu", :password=>"test")
+    ret = @user_from.returns.create(:to_id => @user_to.id)
+    ret.create_item(:name=>"test item")
+
+    @reminder = ret.create_reminder(:frequency=>1, :return_date=>DateTime.now + 3.days)
+
+  end
+
+  test "sent reminder" do
+  	# Send the email, then test that it got queued
+    email = ReminderMailer.reminder_email(@reminder).deliver
+    assert !ActionMailer::Base.deliveries.empty?
+
+    # Test the sent email contains what we expect it to
+    assert_equal [@user_to.email], email.to
+    assert_equal "Reminder: Return Item", email.subject
+  end
 end
