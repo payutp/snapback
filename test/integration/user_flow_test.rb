dca2611967b0030a45278e56f0e5d5a05d14f04b
Payut Pantawongdecha <payut.pan@gmail.com>
2013-05-13 00:07:19 -0400

    x

--- test/integration/user_flow_test.rb
+++ test/integration/user_flow_test.rb
@@ -5,10 +5,4 @@ class UserFlowTest < ActionDispatch::IntegrationTest
 		@u1 = User.create(:email=>"a@mit.edu", :password=>"a")
     @u2 = User.create(:email=>"b@mit.edu", :password=>"ta")
 	end
-
-  test "general flow" do
-  	post_via_redirect "/login"
-  	assert_equal '/', path
-  	assert_equal @u1.id, session[:user_id]
-  end
 end
